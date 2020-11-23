e00ccff6a4fc688e4c257d892d2c4f6142c6b0bd
Eorclus <aldzikridp@gmail.com>
2020-11-23 16:09:46 +0700

    hapus config dari login html

--- config.php
+++ config.php
@@ -1,15 +1,3 @@
 <?php
-$dbopts = parse_url(getenv('DATABASE_URL'));
-$connection ->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
-               array(
-                'pdo.server' => array(
-                   'driver'   => 'pgsql',
-                   'user' => $dbopts["eahoyfmznokkdb"],
-                   'password' => $dbopts["29116c045b75e0e039064e2f54a47a040265b0ee395e8eb1ed425190d7c833cb"],
-                   'host' => $dbopts["ec2-54-166-114-48.compute-1.amazonaws.com"],
-                   'port' => $dbopts["5432"],
-                   'dbname' => ltrim($dbopts["path"],'/')
-                   )
-               )
-);
+$conn = pg_connect(getenv("postgres://eahoyfmznokkdb:29116c045b75e0e039064e2f54a47a040265b0ee395e8eb1ed425190d7c833cb@ec2-54-166-114-48.compute-1.amazonaws.com:5432/d78vvls71tq6c"));
 ?>
--- login.php
+++ login.php
@@ -1,10 +1,10 @@
 <?php
     session_start();
-    include('config.php');
+    $conn = pg_connect(getenv("postgres://eahoyfmznokkdb:29116c045b75e0e039064e2f54a47a040265b0ee395e8eb1ed425190d7c833cb@ec2-54-166-114-48.compute-1.amazonaws.com:5432/d78vvls71tq6c"));
     if (isset($_POST['login'])) {
         $name = $_POST['name'];
         $password = $_POST['password'];
-        $query = $connection->prepare("SELECT * FROM users WHERE name=:name");
+        $query = $conn->prepare("SELECT * FROM users WHERE name=:name");
         $query->bindParam("name", $name, PDO::PARAM_STR);
         $query->execute();
         $result = $query->fetch(PDO::FETCH_ASSOC);
@@ -30,18 +30,24 @@
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
 <body>
+<div class="container">
   <h2>Login</h2>
   <form method="post">
   
      
+    <div class="form-group" action="">
       <label>name</label>
       <input type="text" class="form-control" id="name" placeholder="Masukkan username" name="name">
+    </div>
     
      
+    <div class="form-group">
       <label>Password</label>
       <input type="password" class="form-control" id="password" placeholder="Masukkan password" name="pasword">
+    </div>
      
     <input type="submit" name="login" class="btn btn-primary" value="login">
   </form>
+</div>
 </body>
 </html>
