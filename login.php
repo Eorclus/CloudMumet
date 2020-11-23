455f5aeac8f68623cc96aa38d2258adda31724c6
Eorclus <aldzikridp@gmail.com>
2020-11-23 16:21:01 +0700

    ganti php

--- login.php
+++ login.php
@@ -1,5 +1,4 @@
 <?php
-    session_start();
     $conn = pg_connect(getenv("postgres://eahoyfmznokkdb:29116c045b75e0e039064e2f54a47a040265b0ee395e8eb1ed425190d7c833cb@ec2-54-166-114-48.compute-1.amazonaws.com:5432/d78vvls71tq6c"));
 if(isset($_POST['submit'])&&!empty($_POST['submit'])){
     
